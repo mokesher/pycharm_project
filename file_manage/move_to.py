435334370a672b1560c24ef7dd66e246dfbcb353
Moke <mokesher@github.com>
2020-10-08 22:09:25 +0800

    1

--- file_manage/move_to.py
+++ file_manage/move_to.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import os, sys, shutil, random
+import os, shutil, random
 
 
 def run(start_path, result_file):
@@ -9,16 +9,18 @@ def run(start_path, result_file):
         if os.path.isfile(file):
             if file == os.path.join(result_file, i):
                 continue
-            # elif i.endswith(".wmv") or i.endswith(".mp4") or i.endswith(".mkv") or i.endswith(".avi") or i.endswith(".MP4") or i.endswith(".mov"):
-            elif i.endswith(".flac") or i.endswith(".mp3"):
+            elif i.lower().endswith(".wmv") or i.lower().endswith(".mp4")\
+                    or i.lower().endswith(".mkv")\
+                    or i.lower().endswith(".avi")\
+                    or i.lower().endswith(".mov"):
+            # elif i.endswith(".flac") or i.endswith(".mp3"):
                 print(file)
                 try:
-                    shutil.copy(file, result_file)
-                    pass
+                    shutil.move(file, result_file)
                 except shutil.Error:
                     name = i.rsplit(".")[0]
                     type = i.rsplit(".")[-1]
-                    new_name = f"{name}-{random.randint(0, 99)}.{type}"
+                    new_name = f"{name}-{random.randint(0, 999)}.{type}"
                     print("rename", new_name)
                     new_file = os.path.join(start_path, new_name)
                     os.rename(file, new_file)
@@ -27,6 +29,6 @@ def run(start_path, result_file):
             run(file, result_file)
 
 
-start_path = r"E:\Github\pycharm_project\Test_All\spider\mp3"
+start_path = r"D:\bz"
 result_file = start_path
 run(start_path, result_file)
